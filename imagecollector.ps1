9a16ebb8a045f7cae4bdde019db242ce91a4ad2b
n00b101 <32572388+n00b101@users.noreply.github.com>
2023-05-23 11:55:14 +0800

    Update imagecollector.ps1

--- imagecollector.ps1
+++ imagecollector.ps1
@@ -1,7 +1,6 @@
-#Read the Wiki for instructions
 # Define the URL and output path for FTK Imager
 $ftkImagerUrl = "https://github.com/alwinux/ftkimager/raw/main/ftkimager.exe"
-$ftkImagerPath = "C:\Program Files\FTKImager\ftkimager.exe" 
+$ftkImagerPath = "C:\Program Files\FTKImager\ftkimager.exe"
 
 # Define the source drive and destination path for the forensic image
 $sourceDrive = "C"
@@ -12,3 +11,10 @@ Invoke-WebRequest -Uri $ftkImagerUrl -OutFile $ftkImagerPath
 
 # Create the forensic image
 & $ftkImagerPath $sourceDrive $destinationPath --e01 --frag 10GB
+
+# Display completion message
+Write-Host "Forensic image creation completed."
+
+# Prompt the user to press any key before exiting
+Write-Host "Press any key to exit..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
